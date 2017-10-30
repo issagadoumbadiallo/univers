e5a2c04a36f9550455e40c5cdf710278a909720a
issagadoumbadiallo <issagadoumbadiallo@gmail.com>
2017-10-30 02:23:55 +0000

    adding climbingthehill

--- adouna/bracket.hpp
+++ adouna/bracket.hpp
@@ -77,7 +77,7 @@ public:
 	
 	auto size(){return bracketsPool.size();}
 };
-
+// this have to be put outside and a test have to be built
 auto Parsebracket(std::string stringsequence){
 	auto NextOpenbracket=1;
 	auto cPosition=1;
@@ -105,13 +105,11 @@ auto Parsebracket(std::string stringsequence){
 
 	//Found brackets cout
 	std::cout<<Foundbrackets.size() <<" blocks found :" << '\n';
-	for(int i=1;i<=Foundbrackets.size();i++){
+	for(uint i=1;i<=Foundbrackets.size();i++){
 		Foundbrackets[i]->setdata(stringsequence);
 		
 		Foundbrackets[i]->value();
 	}
-	//std::cout<<"test : " << Foundbrackets[1]->startPosition()<<'\n';
-	//std::cout<<" extracted data : " << Foundbrackets[1]->extractData(Foundbrackets[2])<<'\n';
-	
+
 	return 1;
 }
--- examples/parsebracket/parsebracket.cpp
+++ examples/parsebracket/parsebracket.cpp
@@ -1,6 +1,6 @@
 #include "bracket.hpp"
 
 int main(){
-	auto Parsed=Parsebracket("(moi(nice())fatigu)(last)");
-	return 1;
+	return Parsebracket("(moi(nice())fatigu)(last)");
+;
 }
